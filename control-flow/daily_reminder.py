e00ada7d97323e65a8a8b1b08ba3cc40b953f379
Menna Seif <menna.seif.122@gmail.com>
2025-06-14 02:28:22 +0300

    Fix input prompts and update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 task = input ("Enter the task: ")
-priority = input ("Priority (high/medium/low): ").lower()
-time_bound =input ("Is it time bound? (yes/no): ").lower()
+priority = input ("Priority (high/medium/low): ")
+time_bound =input ("Is it time bound? (yes/no): ")
 
 match priority:
     case "hight":
